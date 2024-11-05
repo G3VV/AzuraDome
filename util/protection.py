3248a1b836fdf0607329a9e3a5d70c1967a437b9
Archie Williams <46306494+G3VV@users.noreply.github.com>
2024-11-05 05:37:42 +0000

    added connected time, will expand on it more

--- util/protection.py
+++ util/protection.py
@@ -9,31 +9,35 @@ async def defence(stop_event):
     while not stop_event.is_set():
         data = await getListeners()
         client_ips = {}
+        
         if int(data['icestats']['source']['listeners']) > 1:
-            client_ips = {client['ID']: client['IP'] for client in data['icestats']['source']['listener']}
+            client_ips = {client['ID']: {'IP': client['IP'], 'Connected': client['Connected']} 
+                          for client in data['icestats']['source']['listener']}
             
             unique_ips = {}
             ip_count = {}
             
-            for id, ip in client_ips.items():
+            for id, info in client_ips.items():
+                ip = info['IP']
                 if ip not in ip_count:
                     ip_count[ip] = 0
                 ip_count[ip] += 1
-        
-            for id, ip in client_ips.items():
+            
+            for id, info in client_ips.items():
+                ip = info['IP']
                 if ip_count[ip] > 1:
-                    if ip not in unique_ips.values():
-                        unique_ips[id] = ip
+                    if ip not in [entry['IP'] for entry in unique_ips.values()]:
+                        unique_ips[id] = info
                 else:
-                    unique_ips[id] = ip
-        
+                    unique_ips[id] = info
+            
             ids_to_remove = [id for id in client_ips if id not in unique_ips]
             for id in ids_to_remove:
                 url = icecast_url + "admin/killclient?mount=" + icecast_mount + "&id=" + id
                 async with aiohttp.ClientSession() as session:
                     async with session.get(url, auth=aiohttp.BasicAuth(icecast_admin, icecast_password)) as response:
                         if response.status == 200:
-                            print(f"Disconnected {client_ips[id]} from {icecast_mount}")
+                            print(f"Kicked {client_ips[id]['IP']} connected for {client_ips[id]['Connected']} seconds")
             client_ips = unique_ips
 
         await asyncio.sleep(2)
